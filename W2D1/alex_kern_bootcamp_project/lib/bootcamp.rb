bde452c2abb5fc965e5c08ab6d95a9070e6e9a0a
Alexander Kern <alexander.t.kern@gmail.com>
2022-04-25 13:49:56 -0700

    adding some more of bootcamp project

--- W2D1/alex_kern_bootcamp_project/lib/bootcamp.rb
+++ W2D1/alex_kern_bootcamp_project/lib/bootcamp.rb
@@ -8,5 +8,36 @@ class Bootcamp
         @students = []
         @grades = Hash.new {|h, k|h[k] = []}
     end
+
+    def name
+        @name
+    end
+    
+    def slogan
+        @slogan
+    end
+
+    def teachers
+        @teachers
+    end
+
+    def students
+        @students
+    end
+
+    def hire(teacher_name)
+        @teachers << teacher_name
+    end
+
+    def enroll(student_name)
+        if @students.length < @student_capacity
+            @students << student_name
+            return true
+        else
+            return false
+        end
+    end
+
+
 end
 
