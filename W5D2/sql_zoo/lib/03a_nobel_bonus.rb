84a7cf64faeae5a06b64c0962aa7a8dce9102437
Alexander Kern <alexander.t.kern@gmail.com>
2022-05-17 21:22:21 -0700

    sql classwork commit

---
+++ W5D2/sql_zoo/lib/03a_nobel_bonus.rb
@@ -0,0 +1,24 @@
+# == Schema Information
+#
+# Table name: nobels
+#
+#  yr          :integer
+#  subject     :string
+#  winner      :string
+
+require_relative './sqlzoo.rb'
+
+def physics_no_chemistry
+  # In which years was the Physics prize awarded, but no Chemistry prize?
+  execute(<<-SQL)
+  SELECT
+    DISTINCT yr
+  FROM
+    nobels
+  LEFT JOIN nobels 
+    ON nobels as 'nobel_2'
+    
+
+    
+  SQL
+end
